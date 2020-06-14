b1f5b408275f28ecc98f7d50bd191046b9ef9531
Ahmed Elhor <aelhor90@gmail.com>
2020-06-14 12:48:39 +0200

    Updates

---
+++ precache-manifest.606866848cc57a992ea44f1d81e869b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f2038e7eb478e876de7006f7e7d658",
+    "url": "/Covid_19-Casses/index.html"
+  },
+  {
+    "revision": "653c8ade6ff1e7eb952a",
+    "url": "/Covid_19-Casses/static/css/main.9c5d33e2.chunk.css"
+  },
+  {
+    "revision": "390a16aa05b5ff6934dc",
+    "url": "/Covid_19-Casses/static/js/2.9f16eb33.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Covid_19-Casses/static/js/2.9f16eb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653c8ade6ff1e7eb952a",
+    "url": "/Covid_19-Casses/static/js/main.38a2b3d4.chunk.js"
+  },
+  {
+    "revision": "1650e8252d7e8f4cce6d",
+    "url": "/Covid_19-Casses/static/js/runtime-main.2dff79f7.js"
+  },
+  {
+    "revision": "5db80b9a77f9e2c48197f8c9ee738698",
+    "url": "/Covid_19-Casses/static/media/header.5db80b9a.jpg"
+  }
+]);
